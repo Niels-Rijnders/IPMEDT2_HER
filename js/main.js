63573ab5837319440121ad1bb6590ddd08d813cf
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-10-07 10:08:43 +0200

    transitie toegevoegd voor index.html

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ const overlay = document.getElementsByClassName("boddy_overlay")[0];
 
 for(let i = 0; i < cards.length; i++){
   cards[i].onclick = function (event){
-    overlay.style.width = "100%"
+    overlay.style.height = "100%"
     setTimeout(function(){
-      window.location = "https://dennou-translations.tumblr.com/post/159331691639/violet-evergarden-novel-index";
+      window.location = "main.html";
     },750)
   }
 }
