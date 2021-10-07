197f784655c7718eed63e63a3de4f87507a094ef
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-10-07 10:22:34 +0200

    transitie van index naar main gemaakt.
    verder de js en css bestanden opgesplitst voor de transitie zodat ze hergebruikt kunnen worden en zodat ze makkelijk aan te passen zijn

---
+++ js/main_transitie.js
@@ -0,0 +1,7 @@
+const cards = document.getElementsByClassName("intro_Button");
+const overlay = document.getElementsByClassName("boddy_overlay")[0];
+
+    setTimeout(function(){
+        overlay.style.height = "0%"
+    },750)
+
