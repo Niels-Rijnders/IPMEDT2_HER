a948f2a05d0c7bc91e54a0443d922f934d899458
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-10-11 15:30:07 +0200

    javascript button doorsturen veranderen werkt nog niet

--- js/main.js
+++ js/main.js
@@ -2,10 +2,31 @@ const button = document.getElementsByClassName("intro_Button");
 //zorg dat de knop naar een andere pagina de class intro_Button 
 //heeft en zorg dat die een z index van 1 heeft
 const overlay = document.getElementsByClassName("boddy_overlay")[0];
+const wijk_button = document.getElementById("wijk_Button");
 
 setTimeout(function(){
   overlay.style.height = "0%"
 },750)
+
+function writeText(txt) {
+  var array = txt.split(','), a = array[0], b = array[1];
+  document.getElementById("titel").innerHTML = a;
+  document.getElementById("desc").innerHTML = b;
+  switch (a){
+    case "Wijk 1":
+      wijk_button.attr("onclick", "window.location.href='wijk_1.html';");
+      break;
+    case "Wijk 2":
+      wijk_button.onclick="window.location.href='wijk_2.html';";
+      break;
+    case "Wijk 3":
+      wijk_button.onclick="window.location.href='wijk_3.html';";
+      break;
+    default:
+      wijk_button.onclick="window.location.href='masdfaain.html';";
+      break;
+  }
+}
 
 for(let i = 0; i < button.length; i++){
   button[i].onclick = function (event){
@@ -14,10 +35,4 @@ for(let i = 0; i < button.length; i++){
       window.location = "main.html";
     },750)
   }
-}
-
-function writeText(txt) {
-  var array = txt.split(','), a = array[0], b = array[1];
-  document.getElementById("titel").innerHTML = a;
-  document.getElementById("desc").innerHTML = b;
 }
