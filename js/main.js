e7bcbef9c39ed6e47d3650839dfafad7647e0055
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-10-11 14:58:45 +0200

    de twee js bestanden samengevoegd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
+const button = document.getElementsByClassName("intro_Button");
+//zorg dat de knop naar een andere pagina de class intro_Button 
+//heeft en zorg dat die een z index van 1 heeft
+const overlay = document.getElementsByClassName("boddy_overlay")[0];
+
+setTimeout(function(){
+  overlay.style.height = "0%"
+},750)
+
+for(let i = 0; i < button.length; i++){
+  button[i].onclick = function (event){
+    overlay.style.height = "100%"
+    setTimeout(function(){
+      window.location = "main.html";
+    },750)
+  }
+}
 
 function writeText(txt) {
-    var array = txt.split(','), a = array[0], b = array[1];
-    document.getElementById("titel").innerHTML = a;
-    document.getElementById("desc").innerHTML = b;
+  var array = txt.split(','), a = array[0], b = array[1];
+  document.getElementById("titel").innerHTML = a;
+  document.getElementById("desc").innerHTML = b;
 }
