c961284f76ad969138a26ce83d0c0e41bcf8090f
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-10-11 13:23:28 +0200

    transitie modulair gemaakt zodat deze op elke pagina kan werken als de css en js bestanden zijn gekoppeld

--- js/transitie.js
+++ js/transitie.js
@@ -1,6 +1,10 @@
 const cards = document.getElementsByClassName("intro_Button");
 const overlay = document.getElementsByClassName("boddy_overlay")[0];
 
+setTimeout(function(){
+  overlay.style.height = "0%"
+},750)
+
 for(let i = 0; i < cards.length; i++){
   cards[i].onclick = function (event){
     overlay.style.height = "100%"
