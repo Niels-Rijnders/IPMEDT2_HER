4f72db61969dbb3d8ef44528999ce6633026ade3
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-11-01 14:53:17 +0100

    eind button toegevoegd die werkt na 3 keer klikken en op wijk3.html verschijnt de hint na een tijdje

--- js/wijk_3.js
+++ js/wijk_3.js
@@ -2,6 +2,7 @@ const wijk3_foto = document.getElementById("wijk3_foto_js");
 const wijk3_figcaption = document.getElementById("wijk3_figcaption_js");
 var foto_nummer = 1;
 const protest_audio = document.getElementById("protest_audio_js");
+const secret_tekst = document.getElementById("secret_tekst_js");
 
 function slideshow(){
   setInterval(function(){
@@ -35,30 +36,27 @@ function slideshow(){
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_12.png");
           wijk3_figcaption.innerHTML = "I pray in the name of Jezus, Father!";
           foto_nummer++;
+          secret_tekst.innerHTML = "klik 3 keer op de eindbutton om naar de laatste pagina te gaan";
           break;
         case 7:
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_1.png");
-          wijk3_figcaption.innerHTML = "Come back to god!";
+          wijk3_figcaption.innerHTML = "";
           foto_nummer++;
           break;
         case 8:
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_3.png");
-          wijk3_figcaption.innerHTML = "Come back to god!";
           foto_nummer++;
           break;
         case 9:
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_6.png");
-          wijk3_figcaption.innerHTML = "Come back to god!";
           foto_nummer++;
           break;
         case 10:
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_7.png");
-          wijk3_figcaption.innerHTML = "Come back to god!";
           foto_nummer++;
           break;
         case 11:
           wijk3_foto.setAttribute("src", "img/demonstratie foto's/godsdienst_8.png");
-          wijk3_figcaption.innerHTML = "Come back to god!";
           foto_nummer++;
           break;
         case 12:
