0647dd9bb46819757365f4b4d7e3f637588484ed
s1122255 <74866743+s1122255@users.noreply.github.com>
2021-11-03 11:14:45 +0100

    tekst aan intro pagina toegevoegd en de pagina beschikbaar gemaakt voor de gebruiker

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const button = document.getElementsByClassName("intro_Button");
+const button = document.getElementById("intro_Button");
 //zorg dat de knop naar een andere pagina de class intro_Button
 //heeft en zorg dat die een z index van 1 heeft
 const overlay = document.getElementsByClassName("boddy_overlay")[0];
@@ -39,7 +39,7 @@ for(let i = 0; i < button.length; i++){
   button[i].onclick = function (event){
     overlay.style.height = "100%";
     setTimeout(function(){
-      window.location = "main.html";
+      window.location = "intro.html";
     },750);
   };
 }
