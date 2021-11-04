e5ec780ae0d70bb667b18453b50ae33b846245db
s1122314 <77395264+s1122314@users.noreply.github.com>
2021-11-04 12:44:08 +0100

    Minigame 2 css + js + html aanpassingen

--- js/switch_img.js
+++ js/switch_img.js
@@ -1,6 +1,18 @@
-var button = document.querySelector('#toggler');
+var button1 = document.querySelector('#toggler');
+var button2 = document.querySelector('#toggler2');
+var button3 = document.querySelector('#toggler3');
 
-button.onclick = function(){
+button1.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "img/auto_was_1.jpg"
+}
+
+button2.onclick = function(){
  var img = document.querySelector('img');
  img.src = "img/auto_was_2.jpg"
+}
+
+button3.onclick = function(){
+ var img = document.querySelector('img');
+ img.src = "img/auto_was_3.jpg"
 }
